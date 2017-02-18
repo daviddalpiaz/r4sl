244ac5ef2a3287b5c8d080f78fa903111bc54558
James Joseph Balamuta <balamut2@illinois.edu>
2017-02-18 03:57:11 -0600

    Copies Rmds over to gh-pages for deployment (Closes #3 )

--- deployment/_deploy.sh
+++ deployment/_deploy.sh
@@ -2,18 +2,32 @@
 git config --global user.name "David Dalpiaz"
 git config --global user.email "dalpiaz2@illinois.edu"
 
-# Clone the gh-pages repository
+## File System
+# / (Root)
+# |- data
+# |- deployment
+# |- devel
+# |- images
+# |- *.Rmd
+# |- _book # Newly generated commit
+
+# Clone the repository's gh-pages branch
 git clone -b gh-pages \
   https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
   book-output
-  
-# Change to the gh-page clone book-output directory
+
+# The goal of the gh-pages branch is to contain the contents of _book and all .Rmd files
+ 
+# Change to the gh-page branch known on the file system as book-output
 cd book-output
 
 # Copy generated output to book-output
 cp -r ../_book/* ./
 
-# Add all files to the repo
+# Copy all .Rmd files to book-output
+cp -r *.Rmd ./
+
+# Add all files in book-output to the repo
 git add *
 git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
 git push origin gh-pages
